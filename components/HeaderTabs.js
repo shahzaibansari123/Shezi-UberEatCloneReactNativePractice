26afae48272372145ee9d316ba99ace19c3cc359
shahzaibansari123 <ansarishahzaib567@gmail.com>
2021-12-28 23:01:56 +0500

    working on it

---
+++ components/HeaderTabs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default function HeaderTabs (){
+    return(
+        <View>
+            <Text>Header Tabs</Text>
+            <Text>Header Tabs</Text> 
+        </View>
+    )
+}
